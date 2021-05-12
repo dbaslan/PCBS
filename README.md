b1ba02df78b205146aa058848d5b42911d701858
Deniz Aslan <d_b_aslan@windowslive.com>
2021-05-12 18:29:24 +0300

    finished script, updated readme

--- README.md
+++ README.md
@@ -4,8 +4,48 @@ Final Project: "Audiovisual Illusion"
 - Github Page:  <https://dbaslan.github.io/PCBS-project/>
 - Github Repository: <https://github.com/dbaslan/PCBS-project>
 
-The aim of this project is to replicate the experimental set-up created in [Shams et al. (2000)](https://www.nature.com/articles/35048669) "What you see is what you hear", investigating an audiovisual illusion. 
+This repository contains the script and the data for a project meant to be 
+validated for the class "Programming for the Cognitive and Brain Sciences". 
+The aim of the project is to replicate the experimental 
+set-up created in 
+[Shams et al. (2000)](https://www.nature.com/articles/35048669) 
+"What you see is what you hear", investigating an audiovisual illusion.
 
-The experiment consists of a group of trials in which a combination of audio and visual stimuli are presented to the participant either once or twice in quick succession. The participant must then indicate if the visual stimulus appeared once or twice. However, the audio and visual stimuli do not match each time. Most importantly, there are times when the audio plays twice while the visual stimulus shows up once. These trials are the ones where the participants experience the illusion that the visual stimulus also appeared twice. 
+The experiment consists of a group of trials in which a combination of 
+audio and visual stimuli are presented to the participant either once or 
+multiple times in quick succession. The visual stimulus is a flashing 
+circle while tha audio stimulus is a simple beeping tone. The participant 
+must then indicate how many times the visual stimulus appeared. However, 
+the audio and visual stimuli do not match each othermost of the time. 
+Interestingly, the audio stimulus affects perception of the visual 
+stimulus, leading to an audiovisual illusion.
 
-In the experiment, the visual stimulus is a flashing white disk while tha audio stimulus is a simple beep. 
+The experiment can be started by running the script main.py. By default, it 
+will launch in windowed mode. It can be launched in full-screen mode by 
+commenting out the relevant piece of code in the script (line 27):
+```python
+xp.control.set_develop_mode(True)
+```
+
+All parameters, such as the timing or the size and color of the visual 
+stimulus, can be adjusted by changing the values at the top of the script:
+```python
+number_of_blocks = 5
+circle_size = 100
+circle_color = (255, 255, 255)
+beep_duration = 50
+duration_of_flash = 50
+duration_between_flashes = 50
+```
+Each block consists of 16 trials.
+
+The script is written in Python 3.8.8, and it uses the 
+[expyriment](https://www.expyriment.org/) and 
+[random](https://docs.python.org/3/library/random.html) modules. It has been 
+tested on Windows 10, and runs without issue. In case the experiment fails to 
+start, try to restart Python and run the script once more.
+
+Once the experiment is finished, the results are automatically saved into 
+a csv file. The results from a previous test run can be found in "data.csv".
+
+Last updated: 12.05.2021
---
+++ main.py
@@ -0,0 +1,101 @@
+# File Name: main.py
+# Author: Deniz ASLAN
+# Last Modified: 12.05.2021
+# Python Version: 3.8
+
+import expyriment as xp
+import random
+
+# change these parameters as seen fit:
+number_of_blocks = 5
+circle_size = 100
+circle_color = (255, 255, 255)
+beep_duration = 50
+duration_of_flash = 50
+duration_between_flashes = 50
+
+# do NOT change these parameters unless you know what you are doing:
+stimuli = ["11", "12", "13", "14",
+           "21", "22", "23", "24",
+           "31", "32", "33", "34",
+           "41", "42", "43", "44"]
+key_values = {49: "1", 50: "2", 51: "3", 52: "4"}
+
+exp = xp.design.Experiment(name="Beeps and Flashes")
+
+# comment out this line to make experiment full-screen:
+xp.control.set_develop_mode(True)
+
+xp.control.initialize(exp)
+
+# initialize stimuli and text messages
+beep = xp.stimuli.Tone(beep_duration)
+disk = xp.stimuli.Circle(circle_size, colour=circle_color, anti_aliasing=10)
+blank = xp.stimuli.BlankScreen()
+
+elicit = xp.stimuli.TextScreen("", """How many times did the circle flash? 
+Press 1, 2, 3 or 4.""")
+
+title = xp.stimuli.TextScreen("Beeps and Flashes", """In this experiment, 
+you will be exposed to two stimuli: a flashing circle, and a beeping tone. 
+After each trial, you will be asked to indicate how many times the circle 
+has flashed. Press any key to begin. (You can end the experiment at any time by 
+pressing Escape.)""")
+
+end = xp.stimuli.TextScreen("The End", """The experiment is now over, your 
+answers have been recorded in the file "data.csv". Thank you for your 
+participation. Press any key to exit.""")
+
+beep.preload()
+disk.preload()
+
+# start experiment
+xp.control.start()
+
+title.present()
+exp.keyboard.wait()
+blank.present()
+exp.clock.wait(1000)
+
+# initialize empty set, randomize and present stimuli for specified
+# number of blocks, then collect answers and append to set:
+answers = []
+for block in range(number_of_blocks):
+    random.shuffle(stimuli)
+    for key in stimuli:
+        exp.clock.wait(400)
+        for i in range(4):
+            if int(key[0]) > i:
+                disk.present()
+            if int(key[1]) > i:
+                beep.present()
+            exp.clock.wait(duration_of_flash)
+            blank.present()
+            exp.clock.wait(duration_between_flashes)
+        exp.clock.wait(400)
+        elicit.present()
+        answer, time = exp.keyboard.wait([xp.misc.constants.K_1,
+                                          xp.misc.constants.K_2,
+                                          xp.misc.constants.K_3,
+                                          xp.misc.constants.K_4,])
+        answers.append(key[0])
+        answers.append(key[1])
+        answers.append(key_values[answer])
+        exp.clock.wait(500)
+
+# create csv file, add header, write experiment results into file
+file = open("data.csv", "w")
+file.write("number of flashes,number of beeps,perceived flashes")
+for index, answer in enumerate(answers):
+    if index % 3 == 0:
+        file.write("\n")
+    file.write(answer)
+    if index % 3 != 2:
+        file.write(",")
+file.close()
+
+end.present()
+exp.keyboard.wait()
+
+# end experiment and close window
+xp.control.end()
